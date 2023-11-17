07aa169778bafe6b3ac889870ce08d8fba22e1e3
plurpio <nicolaialassov@icloud.com>
2023-11-17 23:25:12 +1100

    added nvim options

--- config/nvim/lua/general.lua
+++ config/nvim/lua/general.lua
@@ -1,17 +1,22 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
 vim.opt.cursorline = true
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.opt.scrolloff = 8
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.cmd.colorscheme "catppuccin-macchiato"
 vim.cmd.AirlineTheme "catppuccin"
+vim.opt.termguicolors = true
