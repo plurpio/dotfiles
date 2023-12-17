e91b42a874981b5014d95d6a75857fad2ddd0d2c
Plurpio <nicolaialassov@icloud.com>
2023-12-17 19:03:05 +1100

    Made changes to nvimtree

--- config/nvim/lua/plugconf/general.lua
+++ config/nvim/lua/plugconf/general.lua
@@ -1,5 +1,4 @@
 -- For plugins with 1 line setups
 require('gitsigns').setup()
 require("telescope").load_extension('harpoon')
-require("nvim-tree").setup()
 require("luasnip.loaders.from_vscode").lazy_load()
