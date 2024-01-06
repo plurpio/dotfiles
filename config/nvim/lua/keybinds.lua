f6425f5416a2241863379be5b6b0098e82e7ea5d
Plurpio <nicolaialassov@icloud.com>
2024-01-06 17:41:11 +1100

    Neovim keybind changes & zshenv changes

--- config/nvim/lua/keybinds.lua
+++ config/nvim/lua/keybinds.lua
@@ -4,13 +4,13 @@
 
 vim.g.mapleader = ' '
 
-vim.keymap.set("n", "ew", "<cmd>w <CR>") -- Save file
-vim.keymap.set("n", "eq", "<cmd>wq <CR>") -- Save and quit file
+vim.keymap.set("n", "<leader>w", "<cmd>w <CR>") -- Save file
+vim.keymap.set("n", "<leader>q", "<cmd>wq <CR>") -- Save and quit file
 vim.cmd "map ec :lua require('conform').format() <Enter>"
 
 -- Git
-vim.keymap.set("n", "eg", "<cmd>Neogit <CR>") -- Opens NeoGit
-vim.keymap.set("n", "ef", "<cmd>Telescope git_files <CR>") -- Searchs files in git repos
+vim.keymap.set("n", "<leader>g", "<cmd>Neogit <CR>") -- Opens NeoGit
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope git_files <CR>") -- Searchs files in git repos
 
 -- Editor Movement
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -19,14 +19,14 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Navigation
 vim.cmd "map et :lua require('harpoon.mark').add_file() <Enter>" -- Add file to harpoon
 vim.cmd "map er :lua require('harpoon.mark').rm_file() <Enter>" -- Add remove to harpoon
-vim.keymap.set("n", "ex", "<cmd>Dashboard <CR>") -- Access dashboard
-vim.keymap.set("n", "ee", "<cmd>Telescope harpoon marks <CR>") -- Opens marked files changer thing
+vim.keymap.set("n", "<leader>x", "<cmd>Dashboard <CR>") -- Access dashboard
+vim.keymap.set("n", "<leader>e", "<cmd>Telescope harpoon marks <CR>") -- Opens marked files changer thing
 
-vim.keymap.set("n", "<C-f>", "<cmd>Telescope live_grep <CR>") -- Search in file
-vim.keymap.set("n", "<C-t>", "<cmd>Telescope colorscheme <CR>") -- Change colorscheme
+vim.keymap.set("n", "<leader>a", "<cmd>Telescope live_grep <CR>") -- Search in file
+vim.keymap.set("n", "<leader>t", "<cmd>Telescope colorscheme <CR>") -- Change colorscheme
 
 -- NvimTree
-vim.keymap.set("n", "ed", "<cmd>NvimTreeToggle <CR>") -- Opens dir tree
+vim.keymap.set("n", "<leader>d", "<cmd>NvimTreeToggle <CR>") -- Opens dir tree
 
 -- Learning Binds
 vim.keymap.set("n", "<up>", "")
