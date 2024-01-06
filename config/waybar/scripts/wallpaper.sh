95f7b3d0bd3341a80c7f0dd7624c88f0a06d378a
Plurpio <nicolaialassov@icloud.com>
2024-01-06 20:55:37 +1100

    stihdfoghdf igh

--- config/waybar/scripts/wallpaper.sh
+++ config/waybar/scripts/wallpaper.sh
@@ -1,13 +1,15 @@
 a=~/media/backgrounds/$(ls ~/media/backgrounds | tofi --prompt 'What wallpaper do you want to use?')
+wal -i $a
+
 cp $a ~/.cache/wallpaper
-cp $a ~/.config/tofi/colors-tofi
-cp $a ~/.config/eww/colors.scss
+cp ~/.cache/wal/colors-tofi ~/.config/tofi/colors-tofi
+cp ~/.cache/wal/colors.scss ~/.config/eww/colors.scss
 
 hyprctl hyprpaper unload ~/.cache/wallpaper
 hyprctl hyprpaper preload ~/.cache/wallpaper
 hyprctl hyprpaper wallpaper HDMI-A-1,~/.cache/wallpaper
 hyprctl hyprpaper wallpaper eDP-1,~/.cache/wallpaper
 
-killall waybar && wal -i $a
+killall waybar
 notify-send 'Wallpaper' 'Wallpaper and colourscheme changed' -i ~/.cache/wallpaper
 nohup waybar > /dev/null &
