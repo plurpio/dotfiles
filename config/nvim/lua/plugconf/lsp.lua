e4d095c3c277281c53742fb21c4b183979d42ebe
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:37:49 +1100

    Made LSP installation automatic.

--- config/nvim/lua/plugconf/lsp.lua
+++
@@ -1,5 +0,0 @@
-local lsp_zero = require('lsp-zero')
-require('lspconfig').pyright.setup({})
-require'lspconfig'.lua_ls.setup{}
-require'lspconfig'.marksman.setup{}
-require'lspconfig'.cssls.setup{}
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -75,6 +75,15 @@ require("lazy").setup({
 	  dependencies = { "nvim-lua/plenary.nvim" }
   },
 
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+  },
+
 ------------------------
 -- LSP & CODE EDITING --
 ------------------------
