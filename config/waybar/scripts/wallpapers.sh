81aa3db42678faca62dca0f27988d6d454fa131b
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:38:26 +1100

    1 lined the wallpaper script

--- config/waybar/scripts/wallpapers.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-bg=$(ls ~/media/backgrounds | wofi -i --dmenu)
-
-killall waybar
-wal -i ~/media/backgrounds/$bg
-nohup waybar > /dev/null &
-swww img -t grow ~/media/backgrounds/$bg &
-notify-send "Wallpaper" "Changed wallpaper to $bg" -i ~/media/backgrounds/$bg
