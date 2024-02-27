cbdcdd67eddbc66ab2043f1b510ca67af247d51f
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:38:05 +1100

    Removed unwanted formatter

--- config/nvim/lua/plugconf/conform.lua
+++ config/nvim/lua/plugconf/conform.lua
@@ -2,7 +2,7 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
-    python = { "isort", "black" },
+    python = { "isort" },
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
   },
