aa80f24866a5b9400fff538441d408841c953f5c
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:39:47 +1100

    Made neovim background transparent

--- config/nvim/lua/general.lua
+++ config/nvim/lua/general.lua
@@ -19,3 +19,5 @@ vim.opt.incsearch = true
 
 vim.cmd.colorscheme "catppuccin-macchiato"
 vim.opt.termguicolors = true
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
