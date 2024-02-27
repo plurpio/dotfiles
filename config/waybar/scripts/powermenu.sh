13d02399ba381e2d07eb57dd6448f356e49e9ab8
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:38:17 +1100

    Updated swaylock command in powermenu.sh for waybar

--- config/waybar/scripts/powermenu.sh
+++ config/waybar/scripts/powermenu.sh
@@ -9,8 +9,8 @@ menu="$lock\n$restart\n$shutdown"
 selected="$(echo -e "$menu" | wofi -i -p "What do you want to do?" --matching=fuzzy --dmenu)"
 
 case $selected in
-    $lock)
-      swaylock
+    $lock)      
+    swaylock -f -e -S --grace 2 --fade-in 0.3 --effect-blur 25x25 --indicator --ring-color 9447ca --indicator-radius 100 --indicator-thickness 13 --effect-greyscale
       ;;
     $restart)
       systemctl reboot
