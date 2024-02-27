9e0aa23861c46a0d7aa4cea1db9794cecfa766d1
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:37:51 +1100

    Changed keybinds & added learning keybinds

--- config/nvim/lua/keybinds.lua
+++ config/nvim/lua/keybinds.lua
@@ -4,24 +4,30 @@
 
 vim.g.mapleader = ' '
 
-vim.keymap.set("n", "<C-s>", "<cmd>w <CR>") -- Save file
-vim.keymap.set("n", "<C-q>", "<cmd>wq <CR>") -- Save and quit file
-vim.keymap.set("n", "<S-x>", "<cmd>Dashboard <CR>") -- Access dashboard
-vim.cmd "map <C-q> :lua require('conform').format() <Enter>"
+vim.keymap.set("n", "eg", "<cmd>Neogit <CR>") -- Save file
+vim.keymap.set("n", "ew", "<cmd>w <CR>") -- Save file
+vim.keymap.set("n", "eq", "<cmd>wq <CR>") -- Save and quit file
+vim.cmd "map ec :lua require('conform').format() <Enter>"
 
 -- Editor Movement
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Telescope
+-- Navigation
+vim.cmd "map et :lua require('harpoon.mark').add_file() <Enter>" -- Add file to harpoon
+vim.cmd "map er :lua require('harpoon.mark').rm_file() <Enter>" -- Add remove to harpoon
+vim.keymap.set("n", "ex", "<cmd>Dashboard <CR>") -- Access dashboard
+vim.keymap.set("n", "ee", "<cmd>Telescope harpoon marks <CR>") -- Opens marked files changer thing
+
 vim.keymap.set("n", "<C-f>", "<cmd>Telescope live_grep <CR>") -- Search in file
 vim.keymap.set("n", "<C-t>", "<cmd>Telescope colorscheme <CR>") -- Change colorscheme
-
--- Harpoon
-vim.cmd "map <C-y> :lua require('harpoon.mark').add_file() <Enter>" -- Add file to harpoon
-vim.cmd "map <C-g> :lua require('harpoon.mark').rm_file() <Enter>" -- Add remove to harpoon
-vim.keymap.set("n", "<C-e>", "<cmd>Telescope harpoon marks <CR>") -- Opens marked files changer thing
 
 -- NvimTree
 vim.keymap.set("n", "<C-d>", "<cmd>NvimTreeToggle<CR>") -- Opens dir tree
 vim.keymap.set("n", "<S-d>", "<cmd>NvimTreeFocus<CR>") -- Focuses dir tree
+
+-- Learning Binds
+vim.keymap.set("n", "<up>", "")
+vim.keymap.set("n", "<down>", "")
+vim.keymap.set("n", "<right>", "")
+vim.keymap.set("n", "<left>", "")
