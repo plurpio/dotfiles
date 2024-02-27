41bae0eec9779dfdfbfb8a0a1dc49e15c616e2a3
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:38:28 +1100

    Updated update script

--- config/waybar/scripts/updatesys.sh
+++ config/waybar/scripts/updatesys.sh
@@ -2,7 +2,7 @@
 
 echo "Updates"
 notify-send "Updating system" "Updating offical (pacman), AUR and flatpak packages"
-sudo pacman -Syu
-paru -Syu
-flatpak update
+sudo pacman -Syu --noconfirm
+paru -Syu --noconfirm
+flatpak update --assumeyes
 notify-send "Updates done" "Offical, AUR and flatpak packages have been updated to their latest versions"
