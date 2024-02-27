6d933f81b85a42754c991f71c139c5afe5774e95
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:40:12 +1100

    added rose pine colourschemes and brightness.sh

---
+++ config/hypr/scripts/brightness.sh
@@ -0,0 +1,309 @@
+#! /bin/bash
+
+option=$(echo -e "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59\n60\n61\n62\n63\n64\n65\n66\n67\n68\n69\n70\n71\n72\n73\n74\n75\n76\n77\n78\n79\n80\n81\n82\n83\n84\n85\n86\n87\n88\n89\n90\n91\n92\n93\n94\n95\n96\n97\n98\n99\n100" | tofi --prompt "Brightness")
+
+case $option in
+  0)
+    brightnessctl s 0%
+    ;;
+  1)
+    brightnessctl s 1%
+    ;;
+  2)
+    brightnessctl s 2%
+    ;;
+  3)
+    brightnessctl s 3%
+    ;;
+  4)
+    brightnessctl s 4%
+    ;;
+  5)
+    brightnessctl s 5%
+    ;;
+  6)
+    brightnessctl s 6%
+    ;;
+  7)
+    brightnessctl s 7%
+    ;;
+  8)
+    brightnessctl s 8%
+    ;;
+  9)
+    brightnessctl s 9%
+    ;;
+  10)
+    brightnessctl s 10%
+    ;;
+  11)
+    brightnessctl s 11%
+    ;;
+  12)
+    brightnessctl s 12%
+    ;;
+  13)
+    brightnessctl s 13%
+    ;;
+  14)
+    brightnessctl s 14%
+    ;;
+  15)
+    brightnessctl s 15%
+    ;;
+  16)
+    brightnessctl s 16%
+    ;;
+  17)
+    brightnessctl s 17%
+    ;;
+  18)
+    brightnessctl s 18%
+    ;;
+  19)
+    brightnessctl s 19%
+    ;;
+  20)
+    brightnessctl s 20%
+    ;;
+  21)
+    brightnessctl s 21%
+    ;;
+  22)
+    brightnessctl s 22%
+    ;;
+  23)
+    brightnessctl s 23%
+    ;;
+  24)
+    brightnessctl s 24%
+    ;;
+  25)
+    brightnessctl s 25%
+    ;;
+  26)
+    brightnessctl s 26%
+    ;;
+  27)
+    brightnessctl s 27%
+    ;;
+  28)
+    brightnessctl s 28%
+    ;;
+  29)
+    brightnessctl s 29%
+    ;;
+  30)
+    brightnessctl s 30%
+    ;;
+  31)
+    brightnessctl s 31%
+    ;;
+  32)
+    brightnessctl s 32%
+    ;;
+  33)
+    brightnessctl s 33%
+    ;;
+  34)
+    brightnessctl s 34%
+    ;;
+  35)
+    brightnessctl s 35%
+    ;;
+  36)
+    brightnessctl s 36%
+    ;;
+  37)
+    brightnessctl s 37%
+    ;;
+  38)
+    brightnessctl s 38%
+    ;;
+  39)
+    brightnessctl s 39%
+    ;;
+  40)
+    brightnessctl s 40%
+    ;;
+  41)
+    brightnessctl s 41%
+    ;;
+  42)
+    brightnessctl s 42%
+    ;;
+  43)
+    brightnessctl s 43%
+    ;;
+  44)
+    brightnessctl s 44%
+    ;;
+  45)
+    brightnessctl s 45%
+    ;;
+  46)
+    brightnessctl s 46%
+    ;;
+  47)
+    brightnessctl s 47%
+    ;;
+  48)
+    brightnessctl s 48%
+    ;;
+  49)
+    brightnessctl s 49%
+    ;;
+  50)
+    brightnessctl s 50%
+    ;;
+  51)
+    brightnessctl s 51%
+    ;;
+  52)
+    brightnessctl s 52%
+    ;;
+  53)
+    brightnessctl s 53%
+    ;;
+  54)
+    brightnessctl s 54%
+    ;;
+  55)
+    brightnessctl s 55%
+    ;;
+  56)
+    brightnessctl s 56%
+    ;;
+  57)
+    brightnessctl s 57%
+    ;;
+  58)
+    brightnessctl s 58%
+    ;;
+  59)
+    brightnessctl s 59%
+    ;;
+  60)
+    brightnessctl s 60%
+    ;;
+  61)
+    brightnessctl s 61%
+    ;;
+  62)
+    brightnessctl s 62%
+    ;;
+  63)
+    brightnessctl s 63%
+    ;;
+  64)
+    brightnessctl s 64%
+    ;;
+  65)
+    brightnessctl s 65%
+    ;;
+  66)
+    brightnessctl s 66%
+    ;;
+  67)
+    brightnessctl s 67%
+    ;;
+  68)
+    brightnessctl s 68%
+    ;;
+  69)
+    brightnessctl s 69%
+    ;;
+  70)
+    brightnessctl s 70%
+    ;;
+  71)
+    brightnessctl s 71%
+    ;;
+  72)
+    brightnessctl s 72%
+    ;;
+  73)
+    brightnessctl s 73%
+    ;;
+  74)
+    brightnessctl s 74%
+    ;;
+  75)
+    brightnessctl s 75%
+    ;;
+  76)
+    brightnessctl s 76%
+    ;;
+  77)
+    brightnessctl s 77%
+    ;;
+  78)
+    brightnessctl s 78%
+    ;;
+  79)
+    brightnessctl s 79%
+    ;;
+  80)
+    brightnessctl s 80%
+    ;;
+  81)
+    brightnessctl s 81%
+    ;;
+  82)
+    brightnessctl s 82%
+    ;;
+  83)
+    brightnessctl s 83%
+    ;;
+  84)
+    brightnessctl s 84%
+    ;;
+  85)
+    brightnessctl s 85%
+    ;;
+  86)
+    brightnessctl s 86%
+    ;;
+  87)
+    brightnessctl s 87%
+    ;;
+  88)
+    brightnessctl s 88%
+    ;;
+  89)
+    brightnessctl s 89%
+    ;;
+  90)
+    brightnessctl s 90%
+    ;;
+  91)
+    brightnessctl s 91%
+    ;;
+  92)
+    brightnessctl s 92%
+    ;;
+  93)
+    brightnessctl s 93%
+    ;;
+  94)
+    brightnessctl s 94%
+    ;;
+  95)
+    brightnessctl s 95%
+    ;;
+  96)
+    brightnessctl s 96%
+    ;;
+  97)
+    brightnessctl s 97%
+    ;;
+  98)
+    brightnessctl s 98%
+    ;;
+  99)
+    brightnessctl s 99%
+    ;;
+  100)
+    brightnessctl s 100%
+    ;;
+esac
