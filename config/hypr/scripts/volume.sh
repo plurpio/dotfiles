54bc96a9d990377ecc40ec28acbd292ef6e8f200
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:39:50 +1100

    Added volume script

---
+++ config/hypr/scripts/volume.sh
@@ -0,0 +1,300 @@
+#! /bin/bash
+
+option=$(echo -e "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59\n60\n61\n62\n63\n64\n65\n66\n67\n68\n69\n70\n71\n72\n73\n74\n75\n76\n77\n78\n79\n80\n81\n82\n83\n84\n85\n86\n87\n88\n89\n90\n91\n92\n93\n94\n95\n96\n97\n98\n99\n100" | tofi --prompt "What volume level do you want?")
+
+case $option in
+  0)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 0
+    ;;
+  1)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 1
+    ;;
+  5)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5
+    ;;
+  6)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 6
+    ;;
+  7)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 7
+    ;;
+  8)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 8
+    ;;
+  9)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 9
+    ;;
+  10)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 10
+    ;;
+  11)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 11
+    ;;
+  12)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 12
+    ;;
+  13)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 13
+    ;;
+  14)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 14
+    ;;
+  15)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 15
+    ;;
+  16)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 16
+    ;;
+  17)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 17
+    ;;
+  18)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 18
+    ;;
+  19)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 19
+    ;;
+  20)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 20
+    ;;
+  21)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 21
+    ;;
+  22)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 22
+    ;;
+  23)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 23
+    ;;
+  24)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 24
+    ;;
+  25)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 25
+    ;;
+  26)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 26
+    ;;
+  27)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 27
+    ;;
+  28)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 28
+    ;;
+  29)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 29
+    ;;
+  30)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 30
+    ;;
+  31)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 31
+    ;;
+  32)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 32
+    ;;
+  33)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 33
+    ;;
+  34)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 34
+    ;;
+  35)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 35
+    ;;
+  36)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 36
+    ;;
+  37)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 37
+    ;;
+  38)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 38
+    ;;
+  39)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 39
+    ;;
+  40)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 40
+    ;;
+  41)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 41
+    ;;
+  42)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 42
+    ;;
+  43)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 43
+    ;;
+  44)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 44
+    ;;
+  45)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 45
+    ;;
+  46)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 46
+    ;;
+  47)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 47
+    ;;
+  48)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 48
+    ;;
+  49)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 49
+    ;;
+  50)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 50
+    ;;
+  51)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 51
+    ;;
+  52)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 52
+    ;;
+  53)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 53
+    ;;
+  54)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 54
+    ;;
+  55)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 55
+    ;;
+  56)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 56
+    ;;
+  57)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 57
+    ;;
+  58)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 58
+    ;;
+  59)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 59
+    ;;
+  60)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 60
+    ;;
+  61)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 61
+    ;;
+  62)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 62
+    ;;
+  63)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 63
+    ;;
+  64)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 64
+    ;;
+  65)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 65
+    ;;
+  66)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 66
+    ;;
+  67)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 67
+    ;;
+  68)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 68
+    ;;
+  69)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 69
+    ;;
+  70)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 70
+    ;;
+  71)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 71
+    ;;
+  72)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 72
+    ;;
+  73)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 73
+    ;;
+  74)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 74
+    ;;
+  75)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 75
+    ;;
+  76)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 76
+    ;;
+  77)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 77
+    ;;
+  78)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 78
+    ;;
+  79)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 79
+    ;;
+  80)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 80
+    ;;
+  81)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 81
+    ;;
+  82)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 82
+    ;;
+  83)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 83
+    ;;
+  84)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 84
+    ;;
+  85)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 85
+    ;;
+  86)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 86
+    ;;
+  87)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 87
+    ;;
+  88)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 88
+    ;;
+  89)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 89
+    ;;
+  90)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 90
+    ;;
+  91)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 91
+    ;;
+  92)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 92
+    ;;
+  93)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 93
+    ;;
+  94)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 94
+    ;;
+  95)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 95
+    ;;
+  96)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 96
+    ;;
+  97)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 97
+    ;;
+  98)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 98
+    ;;
+  99)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 99
+    ;;
+  100)
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 100
+    ;;
+esac
