5d50073e775d869bcb695f4a9c37f083d4175495
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-27 17:37:45 +1100

    Added dashboard

---
+++ config/nvim/lua/plugconf/dashboard.lua
@@ -0,0 +1,41 @@
+require('dashboard').setup ({
+     theme = 'hyper',
+    config = {
+      shortcut = {
+        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+        {
+          icon = ' ',
+          icon_hl = '@variable',
+          desc = 'Files',
+          group = 'Label',
+          action = 'Telescope find_files',
+          key = 'f',
+        },
+        {
+          desc = ' dotfiles',
+          group = 'Number',
+          action = 'edit ~/repos/dotfiles/',
+          key = 'd',
+        },
+        {
+          desc = '󱠓 colourscheme',
+          group = 'label',
+          action = 'Telescope colorscheme',
+          key = 'c',
+        },
+      },
+    footer = {"  scaring the ever living shit out of vscode users"}
+    },
+  })
+
+vim.g.dashboard_custom_header =[[
+    \'          ▀████▀▄▄              ▄█ ',
+    \'            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ',
+    \'    ▄        █          ▀▀▀▀▄  ▄▀  ',
+    \'   ▄▀ ▀▄      ▀▄              ▀▄▀  ',
+    \'  ▄▀    █     █▀   ▄█▀▄      ▄█    ',
+    \'  ▀▄     ▀▄  █     ▀██▀     ██▄█   ',
+    \'   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ',
+    \'    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ',
+    \'   █   █  █      ▄▄           ▄▀   ',
+    \]]
