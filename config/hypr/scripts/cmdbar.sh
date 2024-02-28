869467e349db0dac0d1ac80a551a88230f7e353f
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-02-28 23:51:01 +1100

    added prompt script directly in cmdbar.sh

--- config/hypr/scripts/cmdbar.sh
+++ config/hypr/scripts/cmdbar.sh
@@ -1,54 +1,54 @@
 #! /bin/bash
 
+function prompt() {
+  case $(echo -e "Yes\nNo" | tofi --prompt "Action") in 
+    Yes)
+      $($@);; esac
+}
+
 SCRIPTDIR=~/.config/hypr/scripts
 
 option=$(echo -e "Wallpaper\n"\
 "Volume\n"\
 "VMs\n"\
-"Power\n"\
-"Lock\n"\
 "Battery\n"\
 "Update\n"\
-"Logout\n"\
 "Games\n"\
 "Brightness\n"\
-"Theme" | tofi --prompt "What would you like to do?")
+"Theme\n"\
+"Lock\n"\
+"Logout\n"\
+"Suspend\n"\
+"Restart\n"\
+"Shutdown\n" | tofi --prompt "What would you like to do?")
 
 case $option in
-  Volume)
-    sh $SCRIPTDIR/volume.sh
-    ;;
-  Wallpaper)
-    sh $SCRIPTDIR/wallpaper.sh
-    ;;
-  VMs)
-    sh $SCRIPTDIR/virt.sh
-    ;;
-  Power)
-    sh $SCRIPTDIR/power.sh
-    ;;
-  Battery)
-    sh $SCRIPTDIR/battery.sh
-    ;;
-  Update)
-    kitty -e sh $SCRIPTDIR/update.sh
-    ;;
-  Brightness)
-    sh $SCRIPTDIR/brightness.sh
-    ;;
   Logout)
-    pkill -u nico
-    ;;
+    prompt pkill -u nico;;
+  Suspend)
+    prompt systemctl suspend;;
+  Restart)
+    prompt systemctl restart;;
+  Shutdown)
+    prompt systemctl shutdown;;
+  Volume)
+    sh $SCRIPTDIR/volume.sh;;
+  Wallpaper)
+    sh $SCRIPTDIR/wallpaper.sh;;
+  VMs)
+    sh $SCRIPTDIR/virt.sh;;
+  Battery)
+    sh $SCRIPTDIR/battery.sh;;
+  Update)
+    kitty -e sh $SCRIPTDIR/update.sh;;
+  Brightness)
+    sh $SCRIPTDIR/brightness.sh;;
   Games)
-    sh $SCRIPTDIR/games.sh
-    ;;
+    sh $SCRIPTDIR/games.sh;;
   Lock)
-    swaylock -f -e -S --grace 2 --fade-in 0.3 --effect-blur 25x25 --indicator --ring-color 9447ca --indicator-radius 100 --indicator-thickness 13 --effect-greyscale
-    ;;
+    prompt swaylock -f -e --image ~/.cache/wallpaper --grace 2 --fade-in 0.3 --indicator --ring-color 9447ca --indicator-radius 100 --indicator-thickness 25 --effect-vignette 0.7:0.7 --effect-pixelate 20;;
    Theme)
-    theme=$(/bin/ls $HOME/.config/hypr/themes | tofi --prompt "What theme do you want?") && [ -n "$theme" ] && for file in "$HOME/.config/hypr/themes"/*; do sed -i "s|$(basename "$file")|$theme|g" $HOME/.config/hypr/hyprland.conf; done && notify-send "Theme" "Theme was changed to $theme."
-    ;;
+    theme=$(/bin/ls $HOME/.config/hypr/themes | tofi --prompt "What theme do you want?") && [ -n "$theme" ] && for file in "$HOME/.config/hypr/themes"/*; do sed -i "s|$(basename "$file")|$theme|g" $HOME/.config/hypr/hyprland.conf; done && notify-send "Theme" "Theme was changed to $theme.";;
  "")
-    exit
-    ;;
+    exit;;
 esac
