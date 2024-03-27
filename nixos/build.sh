eba16b0736f2d42cff8f2504a15e065cacfbc309
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-03-27 13:10:54 +1100

    "sudo is bloat" part 2

--- nixos/build.sh
+++ nixos/build.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 echo "saving hardware-configuration.nix"
-sudo cp /etc/nixos/hardware-configuration.nix /tmp/hardware-configuration.nix
+doas cp /etc/nixos/hardware-configuration.nix /tmp/hardware-configuration.nix
 echo "removing old files..."
-sudo rm -rf /etc/nixos/*
+doas rm -rf /etc/nixos/*
 echo "restoring hardware-configuration.nix"
-sudo cp /tmp/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+doas cp /tmp/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 echo "copying new files..."
-for f in *; do sudo cp $PWD/$f /etc/nixos; done
+for f in *; do doas cp $PWD/$f /etc/nixos; done
 echo "rebuilding system..."
-sudo nixos-rebuild switch --flake /etc/nixos#default
+doas nixos-rebuild switch --flake /etc/nixos#default
 echo "copying flake.lock..."
-sudo cp /etc/nixos/flake.lock $PWD/flake.lock
+doas cp /etc/nixos/flake.lock $PWD/flake.lock
